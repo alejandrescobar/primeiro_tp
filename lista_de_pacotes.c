b1a04dfad083ea3f9583a2f877c99ad64572e749
alejandrescobar <alemateu12354@gmail.com>
2025-09-16 13:15:48 -0300

    carregamento de arquivos
    leitura de arquivos e passagem para a função insere pacote final

--- lista_de_pacotes.c
+++ lista_de_pacotes.c
@@ -45,7 +45,9 @@ void ImprimeLista(listaPacotes *lista){
 }
 
 void carregar_arquivos(listaPacotes* lista , const char *nome_do_arquivo)
-{
+{   
+    float peso_maximo_drone;
+    int numero_de_pacotes;
     FILE *arquivo;
     char linha[MAX_linhas];
 
@@ -57,15 +59,47 @@ void carregar_arquivos(listaPacotes* lista , const char *nome_do_arquivo)
         return;
     }
 
-    while (fgets(linha,MAX_linhas,arquivo)!=NULL)
-    {
-        linha[strcspn(linha, "\n")] = 0;//considera a quebra de linha como 0 pois ta lendo tudo 
+        fgets(linha,MAX_linhas,arquivo);//armazena o primeiro valor da linha(pesomaximodrone)
+        linha[strcspn(linha, "\n")] = 0;//tira a quebra de linha
+        peso_maximo_drone =atof(linha);
 
-        if (strlen(linha)>0)
-        {
+        fgets(linha,MAX_linhas,arquivo);//le a segunda linha com numero de pacotes
+        linha[strcspn(linha, "\n")] = 0;
+        numero_de_pacotes=atoi(linha);// atof convert para o tipo int o conteudo das linhas 
+       
+        for (int i = 0;i<numero_de_pacotes;i++){
 
-        }
+        dadospacote novo_pacote;
+        char *conteudo_str;
+        char *destinatario_str;
+        char *peso_str;
+        char *distancia_str;
+        // variaveis para receber os vaores do arquivo 
+        
+        fgets(linha, MAX_linhas, arquivo);//ler linhas do pacote
+        linha[strcspn(linha, "\n")] = 0;
 
+    if (strlen(linha)>0){
 
+        char *conteudo_str = strtok(linha, " ");
+        char *destinatario_str = strtok(NULL," ");
+        char *peso_str=strtok(NULL," ");
+        char *distancia_str = strtok(NULL," ");
+// estamos pegando cada segmento de linha e separando em cada variavel , usando o espaço como requisito de quebra 
+        float peso_convertido = atof(peso_str);
+        float distancia_convertida = atof(distancia_str);//convertendo de char para float 
+
+        strncpy(novo_pacote.conteudo ,conteudo_str,sizeof(novo_pacote.conteudo));//strncpy precisa de 3 parametros destino, conteudo,e o tamanho
+        strncpy(novo_pacote.destinatario,destinatario_str,sizeof(novo_pacote.destinatario));
+        novo_pacote.peso = peso_convertido;
+        novo_pacote.distancia = distancia_convertida;     //estamos passando o valor direto , se fosse ponteiro teriamos que usar as setinhas             
+        InserePacoteFinal(lista,&novo_pacote);
+
+        
+
+    
+                        }
     }
+
+fclose(arquivo);
 }
